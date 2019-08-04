213a834bed2977d08700568f20f18176a0419606
JITESH <jitesh.bits@gmail.com>
2019-08-05 00:54:33 +0530

    git ignore modified

--- phoenix.py
+++ phoenix.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'Welcome to the Deep Learning End Point created by Jitesh Motwani'
+	return 'Welcome to the Deep Learning End Point created by Jitesh Motwani and Karan Dev'
 
 
 @app.route('/deeplearning/api/object_detection',methods = ['POST', 'GET'])
